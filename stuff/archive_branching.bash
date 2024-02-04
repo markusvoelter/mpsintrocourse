38cb1f18f0349ace010a2f8a5cca514f1056d28e
Eugen Schindler <eugen@schindlerweb.net>
2024-02-04 20:29:09 +0100

    Helper script - see it as a first step towards github actions

---
+++ stuff/archive_branching.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+archiveprefix=$1
+archivebranchprefix=archive/$1
+
+cd `dirname $0`/..
+pwd
+
+for i in master 02-statemachine-states-and-a-checking-rule 03a-internal-structure-states-events-transitions 03b-constraints-initialstate-selftransitions 04-second-language-for-testing-statemachines 05-and-06-generators-for-statemachines-and-testcases 08a-expressions-structure 08b-expressions-typechecking 09-expressions-interpreters-and-testexecution
+do
+	echo git checkout $i
+	echo git branch $archivebranchprefix/$i
+	echo git checkout $archivebranchprefix/$i
+	echo git push --set-upstream origin $archivebranchprefix/$i
+done
